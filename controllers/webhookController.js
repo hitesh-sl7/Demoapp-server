56896e844f24e25e50b1a85a13a44ecd8f195c0c
Hitesh Khatana <hitesh.khatana@securelayer7.net>
2023-10-19 16:38:55 +0530

    response fixes

--- controllers/webhookController.js
+++ controllers/webhookController.js
@@ -1,15 +1,18 @@
 
+const alerts = [];
+
 var Webhook = function(){
 };
 Webhook.webhookReceive = async (req, res) => {
     var Reqdata = req.body;
+    alerts.push(Reqdata);
     console.log(Reqdata,"------------- webhook received");
     return res.status(200).send(Reqdata);
 };
 
 Webhook.webhookSend = async (req, res) => {
     var Reqdata = req.body;
-    console.log(Reqdata,"------------- webhook sent");
+    console.log(alerts,"------------- webhook sent");
     return res.status(200).send(Reqdata);
 };
 
