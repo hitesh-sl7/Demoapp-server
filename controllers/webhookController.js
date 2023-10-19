4e289a3b31e8c4cebaa583f3fde2f0d129c8cbbc
Hitesh Khatana <hitesh.khatana@securelayer7.net>
2023-10-19 14:54:57 +0530

    webhook api added

--- controllers/webhookController.js
+++ controllers/webhookController.js
@@ -3,7 +3,7 @@ var Webhook = function(){
 };
 Webhook.webhookLog = async (req, res) => {
     var Reqdata = req.body;
-    console.log(Reqdata);
+    console.log(Reqdata,"------------- webhook response");
     return res.status(200).send(sendData);
 };
 
