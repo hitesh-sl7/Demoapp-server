be212d8ef3f18e662c8677d89364559b9c4b8f29
Hitesh Khatana <hitesh.khatana@securelayer7.net>
2023-10-19 16:02:41 +0530

    webhook implemented

--- controllers/webhookController.js
+++ controllers/webhookController.js
@@ -4,7 +4,7 @@ var Webhook = function(){
 Webhook.webhookLog = async (req, res) => {
     var Reqdata = req.body;
     console.log(Reqdata,"------------- webhook response");
-    return res.status(200).send(sendData);
+    return res.status(200).send(Reqdata);
 };
 
 module.exports = Webhook;
