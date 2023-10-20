1223bc85a11f1ee237448cc47c70b28ffcded21b
Hitesh Khatana <hitesh.khatana@securelayer7.net>
2023-10-20 10:22:22 +0530

    response fix

--- controllers/webhookController.js
+++ controllers/webhookController.js
@@ -11,8 +11,8 @@ Webhook.webhookReceive = async (req, res) => {
 };
 
 Webhook.webhookSend = async (req, res) => {
-    var SendData = {};
     var user_id = req.query['user_id'];
+    var SendData = {"status":"","severity":"","device":{"device_id":"","ip":"","location":", "},"user":{"user_id":str(user_id),"email":"","username":""},"message":""};
     for (let i = alerts.length - 1; i >= 0; i--) {
         if (parseInt(alerts[i]['user']['user_id']) === parseInt(user_id)) {
             SendData = alerts[i];
