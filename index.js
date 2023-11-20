310bc848f66f0005e557ca04768479dc00bcde54
Hitesh Khatana <hitesh.khatana@securelayer7.net>
2023-11-20 15:42:58 +0530

    testing tls handshake

--- index.js
+++ index.js
@@ -65,6 +65,6 @@ server.on('secureConnection', handleSecureConnection);
 
 const PORT = process.env.PORT || 6000;
 
-server.listen(PORT, process.env.bindIP, () => {
+app.listen(PORT, process.env.bindIP, () => {
     console.log("Server is running on port " + PORT);
 });
